d00ef65772351ae37dd27aac7507a6b19472f708
Sajdin Murtić <sajdin_m@hotmail.com>
2022-11-11 19:22:02 +0100

    landing

---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import Home from '../pages/Home'
+import Navbar from './Navbar'
+
+const Landing = ()=>{
+    return(
+        <>
+        <Navbar />
+        <Home />
+        </>
+    )
+}
+
+export default Landing
